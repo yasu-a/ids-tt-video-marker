5ebbffffbe3b1f4ab7d32c7968621acd0396dbae
yasu-a <yasuhiro.gtgt@gmail.com>
2023-10-21 03:37:36 +0900

    enhanced ui

--- main.py
+++ main.py
@@ -125,6 +125,9 @@ class SeekableVideoReader:
 
     @functools.lru_cache(maxsize=int(128e+6 / (1440 * 1040 * 3 * 1 / (2 * 2))))
     def __read(self, i):
+        if self.__prop_frame_index() > i:
+            self.__seek_at(i)
+            self.__grab()
         if self.__prop_frame_index() < i:
             delta = i - self.__prop_frame_index()
             if delta < self.frame_rate * 5:
@@ -133,12 +136,11 @@ class SeekableVideoReader:
             else:
                 self.__seek_at(i)
                 self.__grab()
-        elif self.__prop_frame_index() > i:
-            self.__seek_at(i)
-            self.__grab()
 
         idx = self.__prop_frame_index()
         ts = self.__prop_frame_timestamp()
+
+        assert i == idx, (i, idx)
 
         img = self.__retrieve()
         img = cv2.resize(img, None, fx=0.5, fy=0.5)
@@ -151,6 +153,7 @@ class SeekableVideoReader:
     def __getitem__(self, i):
         i = max(0, min(self.__frame_count - 1, i))
         idx, ts, img = self.__read(i)
+        assert i == idx, (i, idx)
         self.__last_frame_index, self.__last_frame_timestamp = idx, ts
         return img
 
@@ -182,29 +185,35 @@ class ImageViewWidget(QWidget):
         layout = QVBoxLayout()
         self.setLayout(layout)
 
-        label_meta = QLabel(self)
-        layout.addWidget(label_meta)
-        self.__label_meta = label_meta
+        layout_view = QHBoxLayout()
+        layout.addLayout(layout_view)
+
+        layout_view.addStretch(1)
 
         view = QLabel(self, text='IMAGE')
-        layout.addWidget(view)
+        layout_view.addWidget(view)
         self.__view = view
 
+        layout_view.addStretch(1)
+
+        layout_info = QHBoxLayout()
+        layout.addLayout(layout_info)
+
+        layout_info.addStretch(1)
+
         label_info = QLabel(self, text='info')
-        layout.addWidget(label_info)
+        layout_info.addWidget(label_info)
         self.__label_info = label_info
+
+        layout_info.addStretch(1)
 
     def init_video(self, path):
         self.__video_path = path
-        self.update_cap()
-
-    def load_meta(self):
-        self.__video_meta = dict(
-            frame_count=self.__reader.frame_count,
-            frame_rate=self.__reader.frame_rate
-        )
-
-        self.__label_meta.setText('. '.join(f'{k}={v}' for k, v in self.__video_meta.items()))
+        try:
+            self.update_cap()
+        except FileNotFoundError:
+            self.__video_path = None
+            self.__reader = None
 
     def update_cap(self):
         if self.__reader is not None:
@@ -212,15 +221,16 @@ class ImageViewWidget(QWidget):
 
         self.__reader = SeekableVideoReader(self.__video_path)
         self.__current = -1
-        self.load_meta()
 
         self.update_frame(0)
 
     def update_info(self):
         ts, idx = self.__reader.frame_time, self.__reader.frame_index
+        n_fr = self.__reader.frame_count
+        fps = self.__reader.frame_rate
 
         self.__label_info.setText(
-            f'{int(ts) // 60:3d}:{int(ts) % 60:02d}.{(ts - int(ts)) * 1000:03.0f} (#{idx})'
+            f'{int(ts) // 60:3d}:{int(ts) % 60:02d}.{(ts - int(ts)) * 1000:03.0f} ({idx:7d}/{n_fr:7d}) fps={fps:.2f}'
         )
 
         self.changed.emit(idx, ts)
@@ -257,13 +267,13 @@ class ImageViewWidget(QWidget):
         elif key == Qt.Key_E:
             self.set_frame(delta=+1)
         elif key == Qt.Key_A:
-            self.set_frame(delta=-int(self.__video_meta['frame_rate'] / 5))
+            self.set_frame(delta=-int(self.__reader.frame_rate / 5))
         elif key == Qt.Key_D:
-            self.set_frame(delta=+int(self.__video_meta['frame_rate'] / 5))
+            self.set_frame(delta=+int(self.__reader.frame_rate / 5))
         elif key == Qt.Key_Z:
-            self.set_frame(delta=-int(self.__video_meta['frame_rate']))
+            self.set_frame(delta=-int(self.__reader.frame_rate * 10))
         elif key == Qt.Key_C:
-            self.set_frame(delta=+int(self.__video_meta['frame_rate']))
+            self.set_frame(delta=+int(self.__reader.frame_rate * 10))
 
 
 class MarkerViewWidget(QWidget):
@@ -332,7 +342,7 @@ class MarkerViewWidget(QWidget):
         self.load()
 
     def update_view(self, current_frame_index):
-        n_side = 70
+        n_side = 80
         idx = [i for i in range(current_frame_index - n_side, current_frame_index + n_side + 1)]
         lst = []
         for i in idx:
@@ -382,14 +392,14 @@ class MarkerWidget(QWidget):
 
         # source dir
         widget_sd = DirectoryChooserWidget('Source Directory',
-                                           os.path.expanduser('H:/idsttvideos/singles'))
+                                           os.path.expanduser('~/Desktop/idsttvideos/singles'))
         widget_sd.changed.connect(self.update_source_list)
         layout.addWidget(widget_sd)
         self.__widget_sd = widget_sd
 
         # working dir
         widget_wd = DirectoryChooserWidget('Working Directory',
-                                           os.path.expanduser(r'G:\ids-tt-video-maker-working'))
+                                           os.path.expanduser(r'~/Desktop/VideoMarkerData'))
         widget_wd.changed.connect(self.update_source_list)
         layout.addWidget(widget_wd)
         self.__widget_wd = widget_wd
@@ -398,11 +408,10 @@ class MarkerWidget(QWidget):
         layout_sl = QVBoxLayout()
         layout.addLayout(layout_sl)
 
-        list_sl = QListWidget(self)
-        list_sl.setFixedHeight(100)
-        list_sl.clicked.connect(self.list_sl_clicked)
-        layout_sl.addWidget(list_sl)
-        self.__list_sl = list_sl
+        combo_sl = QComboBox(self)
+        combo_sl.currentIndexChanged.connect(self.combo_sl_changed)
+        layout_sl.addWidget(combo_sl)
+        self.__combo_sl = combo_sl
 
         label_meta = QLabel(self)
         layout_sl.addWidget(label_meta)
@@ -463,19 +472,28 @@ class MarkerWidget(QWidget):
                 if j is None:
                     j = self.__widget_img_view.reader.frame_count - 1
                 self.__widget_img_view.set_frame(val=j)
+            elif key == Qt.Key_Period:
+                self.__widget_img_view.set_frame(val=self.__widget_img_view.reader.frame_count - 1)
+            elif key == Qt.Key_Comma:
+                self.__widget_img_view.set_frame(val=0)
 
     def update_source_list(self):
         source_root = self.__widget_sd.path
-        path_lst = os.listdir(source_root)
-        self.__list_sl.clear()
-        if os.path.exists(source_root):
-            for path in path_lst:
-                path = os.path.join(source_root, path)
-                if path.endswith('.mp4') and os.path.isfile(path):
-                    self.__list_sl.addItem(path)
+        try:
+            path_lst = os.listdir(source_root)
+        except FileNotFoundError:
+            self.__combo_sl.clear()
+        else:
+            self.__combo_sl.clear()
+            if os.path.exists(source_root):
+                for path in path_lst:
+                    path = os.path.join(source_root, path)
+                    if path.endswith('.mp4') and os.path.isfile(path):
+                        self.__combo_sl.addItem(path)
 
-    def list_sl_clicked(self):
-        selected_video_path = self.__list_sl.selectedItems()[0].text()
+    @pyqtSlot(int)
+    def combo_sl_changed(self, selected_index):
+        selected_video_path = self.__combo_sl.itemText(selected_index)
         self.__widget_img_view.init_video(selected_video_path)
         self.__widget_marker.update_path(
             os.path.join(self.__widget_wd.path,
@@ -502,7 +520,7 @@ class MainWindow(QMainWindow):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    app.setStyleSheet("*{font-size: 12pt; font-family: Consolas;}")
+    app.setStyleSheet("*{font-size: 11pt; font-family: Consolas;}")
     ew = MainWindow()
     ew.show()
     sys.exit(app.exec_())
